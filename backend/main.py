d6da0a22c5b26681fcb85b563ac48a6b86499879
Rishav Kumar <rishav.iitmadras@gmail.com>
2026-05-22 12:42:11 +0530

    connect backend to live frontend

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="RAG Knowledge Base API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # React dev server
+    allow_origins=["http://localhost:5173", "https://rag-knowledge-base-three.vercel.app/"],  # React dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
